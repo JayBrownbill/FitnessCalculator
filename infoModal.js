0955cd8b851b60c490f8fdc97b18ecb4e510fd24
Jay Brownbill <jamalbrownbill96@googlemail.com>
2021-03-30 19:24:04 +0100

    modal positioning & append display bug fixed.

--- infoModal.js
+++ infoModal.js
@@ -1,12 +1,24 @@
 $(document).ready(function () {
+  //Structured HTML content for modal in variable
+  let modalDiv = $(
+    `<div id="calculatorModal"></div>`
+  );
 
-    let modalDiv = $(`<div id="calculatorModal"> <h2> WHY, WHY, WHY?! </h2> <br> Knowing your personal calorie and protein requirements is the first step to achieving your goals, the JNPT+ calculator produces your intakes using the most reliable formulas to date, ensuring you get off to a strong start.<br/> <br/> <b> (We breakdown the reasons why we used these key nutrients in the nutrition guide)<b> <br> <br> <i> Calorie Calculation Used: Mifflin St-jeor Equation </i> </div>`);
-    function ModalPopup(modalDiv) {  
-        $("body").append (`<div class="modalOverlay"> </div>`);
-        $("body").append (modalDiv);
-        $("body").append ();
-    }
+  function CreateModalElement(modalDiv) {
+    //Appennd modal div to body of HTML document
+    $("body").append(modalDiv);
+    $("#calculatorModal").css("opacity","0")
+    $("#calculatorModal").css("position","absolute")
+     $("#calculatorModal").html(`<h2> WHY, WHY, WHY?! </h2> <br> <p class="modalTxt"> Knowing your personal calorie and protein requirements is the first step to achieving your goals, the JNPT+ calculator produces your intakes using the most reliable formulas to date, ensuring you get off to a strong start. </p><br/> <br/> <b> (We breakdown the reasons why we used these key nutrients in the nutrition guide)<b> <br> <br> <i> Calorie Calculation Used: Mifflin St-jeor Equation </i>`);
+  }
+  // Create modal element
+  CreateModalElement(modalDiv);
+  
+  $("#calculatorModal").modal({
+    //Call modal fade in from external library
+    fadeDuration: 1000,
+    fadeDelay: 0,
+  });
 
-    ModalPopup(modalDiv);
-    // $("#calculatorModal").css("position","absolute");
+ 
 });
